8fa434fb7d7f2df623b66646c88bd09cb2bc19ed
Greg Martinez <greg-martinez44@outlook.com>
2026-05-20 14:12:11 -0700

    added dash to bucket prefix

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket_prefix = var.bucket_prefix
+  bucket_prefix = "${var.bucket_prefix}-"
   region        = var.region
   force_destroy = var.force_destroy
   tags          = var.tags
